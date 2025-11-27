d7d342dc6e121e24bed1610be504c363f0f34190
user <user@WIN-B1U04KLVRPP>
2025-11-27 15:41:08 +0900

    fix:Case14.java 受講生試験の実施結果50点のテストコードの作成
    fix:結合テスト仕様書に追記
    add:evidence/Case16にエビデンスを追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f05_exam/Case14.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f05_exam/Case14.java
@@ -1,8 +1,10 @@
 package jp.co.sss.lms.ct.f05_exam;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,6 +13,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト 試験実施機能
@@ -40,56 +46,264 @@ public class Case14 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// 遷移先URL
+		goTo("http://localhost:8080/lms");
+		// ページタイトルの一致確認
+		String title = WebDriverUtils.webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// 値の取得
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+		final WebElement loginId = webDriver.findElement(By.id("loginId"));
+		final WebElement password = webDriver.findElement(By.id("password"));
+
+		// キー入力
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("studentAA01");
+
+		// ログイン前のエビデンス取得
+		getEvidence(new Object() {
+		}, "before");
+
+		// ログインボタンをクリック
+		login.click();
+
+		// ログイン後のエビデンス取得
+		getEvidence(new Object() {
+		}, "after");
+
+		// 遷移先のURLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/course/detail", url);
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「試験有」の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// テーブルを取得
+		List<WebElement> rows = webDriver.findElements(By.cssSelector(".sctionList tbody tr"));
+
+		// 試験有を探して詳細ボタンを押下
+		for (WebElement row : rows) {
+			final WebElement examCell = row.findElement(By.xpath("./td[4]/span"));
+			if (examCell.getText().equals("試験有")) {
+				final WebElement detailBtn = row.findElement(By.xpath("./td[5]//input[@value='詳細']"));
+				detailBtn.click();
+				break;
+			}
+		}
+
+		// 遷移後のエビデンス取得
+		getEvidence(new Object() {
+		});
+
+		// 試験があるか確認
+		String check = webDriver.findElement(By.xpath("//h3[2]")).getText();
+		assertEquals("本日の試験", check);
+
+		// 遷移先のURLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/section/detail", url);
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「本日の試験」エリアの「詳細」ボタンを押下し試験開始画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		final WebElement detailBtn = webDriver.findElement(By.cssSelector("input[value='詳細']"));
+		detailBtn.click();
+
+		// 遷移後のエビデンス取得
+		getEvidence(new Object() {
+		});
+
+		// 遷移先のURLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/exam/start", url);
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「試験を開始する」ボタンを押下し試験問題画面に遷移")
 	void test05() {
-		// TODO ここに追加
+
+		// ページ最下部までスクロール
+		scrollTo("2000");
+
+		// 試験開始前の過去の試験結果のエビデンス取得
+		getEvidence(new Object() {
+		}, "pastTestResultsBfore");
+
+		// ページ最上部までスクロール
+		scrollTo("10");
+
+		// 遷移前のエビデンス取得
+		getEvidence(new Object() {
+		}, "before");
+
+		final WebElement startBtn = webDriver.findElement(By.cssSelector("input[value='試験を開始する']"));
+		startBtn.click();
+
+		// 遷移後のエビデンス取得
+		getEvidence(new Object() {
+		}, "after");
+
+		// 遷移先のURLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/exam/question", url);
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 正答と誤答が半々で「確認画面へ進む」ボタンを押下し試験回答確認画面に遷移")
 	void test06() {
-		// TODO ここに追加
+
+		// 第１問
+		final WebElement answer01 = webDriver.findElement(By.id("answer-0-2"));
+		answer01.click();
+		getEvidence(new Object() {
+		}, "q1");
+		scrollBy("370");
+
+		// 第２問
+		final WebElement answer02 = webDriver.findElement(By.id("answer-1-2"));
+		answer02.click();
+		getEvidence(new Object() {
+		}, "q2");
+		scrollBy("370");
+
+		// 第３問
+		final WebElement answer03 = webDriver.findElement(By.id("answer-2-0"));
+		answer03.click();
+		getEvidence(new Object() {
+		}, "q3");
+		scrollBy("370");
+
+		// 第４問
+		final WebElement answer04 = webDriver.findElement(By.id("answer-3-0"));
+		answer04.click();
+		getEvidence(new Object() {
+		}, "q4");
+		scrollBy("370");
+
+		// 第５問
+		final WebElement answer05 = webDriver.findElement(By.id("answer-4-1"));
+		answer05.click();
+		getEvidence(new Object() {
+		}, "q5");
+		scrollBy("370");
+
+		// 第６問
+		final WebElement answer06 = webDriver.findElement(By.id("answer-5-1"));
+		answer06.click();
+		getEvidence(new Object() {
+		}, "q6");
+		scrollBy("370");
+
+		// 第７～１２問
+		for (int i = 6; i < 12; i++) {
+			final WebElement answer = webDriver.findElement(By.id("answer-" + i + "-0"));
+			answer.click();
+			getEvidence(new Object() {
+			}, "q" + i);
+			scrollBy("370");
+		}
+
+		// 遷移前のエビデンス取得
+		getEvidence(new Object() {
+		}, "before");
+
+		final WebElement checkBtn = webDriver.findElement(By.cssSelector("input[value='確認画面へ進む']"));
+		checkBtn.click();
+
+		// 遷移後のエビデンス取得
+		getEvidence(new Object() {
+		});
+
+		// 遷移先のURLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/exam/answerCheck", url);
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 「回答を送信する」ボタンを押下し試験結果画面に遷移")
 	void test07() throws InterruptedException {
-		// TODO ここに追加
+
+		// ページ最下部までスクロール
+		scrollTo("5000");
+
+		// 遷移前のエビデンス取得
+		getEvidence(new Object() {
+		}, "before");
+
+		final WebElement sendBtn = webDriver.findElement(By.cssSelector("button[id='sendButton']"));
+		sendBtn.click();
+
+		acceptAlertIfPresent(webDriver);
+
+		// 遷移後のエビデンス取得
+		getEvidence(new Object() {
+		}, "after");
+
+		// 遷移先のURLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/exam/result", url);
 	}
 
 	@Test
 	@Order(8)
 	@DisplayName("テスト08 「戻る」ボタンを押下し試験開始画面に遷移後当該試験の結果が反映される")
 	void test08() {
-		// TODO ここに追加
+
+		// ページ最下部までスクロール
+		scrollTo("5000");
+
+		// 遷移前のエビデンス取得
+		getEvidence(new Object() {
+		}, "before");
+
+		final WebElement backBtn = webDriver.findElement(By.cssSelector("input[value='戻る']"));
+		backBtn.click();
+
+		// 遷移後のエビデンス取得
+		getEvidence(new Object() {
+		}, "after");
+
+		// 遷移先のURLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/exam/start", url);
+		
+		pageLoadTimeout(10);
+
+		// 試験後の過去の試験結果のエビデンス取得
+		getEvidence(new Object() {
+		}, "pastTestResultsAfter");
+
+		// 結果が反映されているかの確認
+		List<WebElement> pastTestResutts = webDriver.findElements(By.xpath("//*[@id=\"main\"]/div/table[2]//td"));
+		boolean found = false;
+		for (WebElement td : pastTestResutts) {
+			if (td.getText().equals("50.0点")) {
+				found = true;
+				break;
+			}
+		}
+		assertTrue(found);
+
 	}
 
 }
