b73bdae0cb9442796a2b4ca4ef76b412ce2e305e
user <user@WIN-B1U04KLVRPP>
2025-11-28 11:45:21 +0900

    fix:Case10.java 受講生勤怠登録正常系のテストコード作成
    fix:結合テスト仕様書に追記
    add:evidence/Case10にエビデンスの追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.thymeleaf.util.StringUtils;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +43,135 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// 遷移先URL
+		goTo("http://localhost:8080/lms");
+
+		// ページタイトルの一致確認
+		String title = WebDriverUtils.webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// 値の取得
+		final WebElement login = webDriver.findElement(By.className("btn-primary"));
+		final WebElement loginId = webDriver.findElement(By.id("loginId"));
+		final WebElement password = webDriver.findElement(By.id("password"));
+
+		// キー入力
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("studentAA01");
+
+		// ログイン前のエビデンス取得
+		getEvidence(new Object() {
+		}, "before");
+
+		// ログインボタンをクリック
+		login.click();
+
+		// ログイン後のエビデンス取得
+		getEvidence(new Object() {
+		}, "after");
+
+		// 遷移先URLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/course/detail", url);
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// リンクを取得し押下
+		final WebElement attendance = webDriver.findElement(By.cssSelector("a[href='/lms/attendance/detail']"));
+		attendance.click();
+
+		// アラート受け入れ処理
+		acceptAlertIfPresent(webDriver);
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
+
+		// 遷移先URLの一致確認
+		String url = webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/attendance/detail", url);
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+
+		// ボタンを取得して押下
+		final WebElement startTime = webDriver.findElement(By.cssSelector("input[value='出勤']"));
+		startTime.click();
+
+		// アラート受け入れ処理
+		acceptAlertIfPresent(webDriver);
+		
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
+
+		// テーブルを取得
+		List<WebElement> rows = webDriver.findElements(By.xpath("//*[@id=\"main\"]/div/table//td"));
+
+		// 本日の日付(2025年11月28日(金))の出勤欄が入力されているか確認
+		boolean found = false;
+		for (WebElement row : rows) {
+			String startTimeText = row.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/table/tbody/tr[2]/td[3]"))
+					.getText();
+			if (StringUtils.isEmpty(startTimeText)) {
+			} else {
+				found = true;
+				break;
+			}
+			assertTrue(found);
+		}
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+		
+		// ボタンを取得して押下
+		final WebElement endTime = webDriver.findElement(By.cssSelector("input[value='退勤']"));
+		endTime.click();
+		
+		// アラート受け入れ処理
+		acceptAlertIfPresent(webDriver);
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
+
+		// テーブルを取得
+		List<WebElement> rows = webDriver.findElements(By.xpath("//*[@id=\"main\"]/div/table//td"));
+
+		// 本日の日付(2025年11月28日(金))の退勤欄が入力されているか確認
+		boolean found = false;
+		for (WebElement row : rows) {
+			String endTimeText = row.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/table/tbody/tr[2]/td[4]"))
+					.getText();
+			if (StringUtils.isEmpty(endTimeText)) {
+			} else {
+				found = true;
+				break;
+			}
+			assertTrue(found);
+		}
 	}
 
 }
